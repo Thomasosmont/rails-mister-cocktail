8f2be56c6423cafd3913612fa1dd96a712056bb3
Julien Pauss <julien.pauss@gmail.com>
2017-04-07 14:09:11 -0400

    Cocktails created

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'pages/home', to: "pages#home"
   root to: "pages#home"
+  mount Attachinary::Engine => "/attachinary"
   resources :ingredients, only: [:show]
   resources :cocktails, except: [:edit, :update, :destroy] do
     resources :doses, only: [:new, :create, :destroy]
