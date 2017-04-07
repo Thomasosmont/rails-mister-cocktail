1d5e9a2f5de2a2c39c25b19cb3e9e1e45365bc83
Julien Pauss <julien.pauss@gmail.com>
2017-04-07 13:33:27 -0400

    front-end improving

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,9 @@
+require 'json'
+require 'open-uri'
 Ingredient.destroy_all
-
-15.times do
-  ingredients = Ingredient.new(
-    name: Faker::Food.ingredient,
-  )
-  ingredients.save!
+url = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+thing_serialized = open(url).read
+thing = JSON.parse(thing_serialized)
+thing["drinks"].each do |d|
+  Ingredient.create(name: d["strIngredient1"])
 end
